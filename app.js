f92742522b6ff73767cfb9db5c5dabad8c585753
Madu Williams <97499846+Prodigy97@users.noreply.github.com>
2023-03-08 09:54:55 +0100

    finished setting up user signin/signup auth for test

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import bodyParser from "body-parser";
+import connectMongoDB from "./config/db.mjs";
+import UserRouter from "./api/userRoutes.mjs";
+
 const app = express(),
     port = process.env.PORT || 3003;
 
-app.get("/", (req, res) => {
-    res.send("Project Is Running on Browser");
-});
+connectMongoDB();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/user", UserRouter);
 
 app.listen(port, () => {
     console.table(`Express server is running & listening on port ${port}`);
