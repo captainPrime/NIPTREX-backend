7a8b96434255d55e4101079629b03c48d57f493e
olashina201 <olashina201@gmail.com>
2023-04-19 05:44:19 +0100

    refactoring

--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -25,83 +25,83 @@ class ProfileRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, authMiddleware, this.profileController.getProfile);
+    this.router.get(`${this.path}`, authMiddleware(['freelancer', 'client']), this.profileController.getProfile);
     /*
     |--------------------------------------------------------------------------
     | Experience Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/experience`, authMiddleware, this.profileController.createExperience);
-    this.router.get(`${this.path}/experience`, authMiddleware, this.profileController.getUserExperience);
-    this.router.put(`${this.path}/experience/:id`, authMiddleware, this.profileController.updateExperience);
-    this.router.delete(`${this.path}/experience/:id`, authMiddleware, this.profileController.deleteExperience);
-    this.router.get(`${this.path}/experience/:id`, authMiddleware, this.profileController.getExperienceById);
+    this.router.post(`${this.path}/experience`, authMiddleware(['freelancer', 'client']), this.profileController.createExperience);
+    this.router.get(`${this.path}/experience`, authMiddleware(['freelancer', 'client']), this.profileController.getUserExperience);
+    this.router.put(`${this.path}/experience/:id`, authMiddleware(['freelancer', 'client']), this.profileController.updateExperience);
+    this.router.delete(`${this.path}/experience/:id`, authMiddleware(['freelancer', 'client']), this.profileController.deleteExperience);
+    this.router.get(`${this.path}/experience/:id`, authMiddleware(['freelancer', 'client']), this.profileController.getExperienceById);
 
     /*
     |--------------------------------------------------------------------------
     | Education Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/education`, authMiddleware, this.educationController.createEducation);
-    this.router.get(`${this.path}/education`, authMiddleware, this.educationController.getUserEducation);
-    this.router.put(`${this.path}/education/:id`, authMiddleware, this.educationController.updateEducation);
-    this.router.delete(`${this.path}/education/:id`, authMiddleware, this.educationController.deleteEducation);
-    this.router.get(`${this.path}/education/:id`, authMiddleware, this.educationController.getEducationById);
+    this.router.post(`${this.path}/education`, authMiddleware(['freelancer', 'client']), this.educationController.createEducation);
+    this.router.get(`${this.path}/education`, authMiddleware(['freelancer', 'client']), this.educationController.getUserEducation);
+    this.router.put(`${this.path}/education/:id`, authMiddleware(['freelancer', 'client']), this.educationController.updateEducation);
+    this.router.delete(`${this.path}/education/:id`, authMiddleware(['freelancer', 'client']), this.educationController.deleteEducation);
+    this.router.get(`${this.path}/education/:id`, authMiddleware(['freelancer', 'client']), this.educationController.getEducationById);
 
     /*
     |--------------------------------------------------------------------------
     | Certification Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/certificate`, authMiddleware, this.certificationController.createCertification);
-    this.router.get(`${this.path}/certificate`, authMiddleware, this.certificationController.getUserCertification);
-    this.router.put(`${this.path}/certificate/:id`, authMiddleware, this.certificationController.updateCertification);
-    this.router.delete(`${this.path}/certificate/:id`, authMiddleware, this.certificationController.deleteCertification);
-    this.router.get(`${this.path}/certificate/:id`, authMiddleware, this.certificationController.getCertificationById);
+    this.router.post(`${this.path}/certificate`, authMiddleware(['freelancer', 'client']), this.certificationController.createCertification);
+    this.router.get(`${this.path}/certificate`, authMiddleware(['freelancer', 'client']), this.certificationController.getUserCertification);
+    this.router.put(`${this.path}/certificate/:id`, authMiddleware(['freelancer', 'client']), this.certificationController.updateCertification);
+    this.router.delete(`${this.path}/certificate/:id`, authMiddleware(['freelancer', 'client']), this.certificationController.deleteCertification);
+    this.router.get(`${this.path}/certificate/:id`, authMiddleware(['freelancer', 'client']), this.certificationController.getCertificationById);
 
     /*
     |--------------------------------------------------------------------------
     | Billing Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/billing`, authMiddleware, this.billingController.createBilling);
-    this.router.get(`${this.path}/billing`, authMiddleware, this.billingController.getUserBilling);
-    this.router.put(`${this.path}/billing`, authMiddleware, this.billingController.updateBilling);
-    this.router.delete(`${this.path}/billing/:id`, authMiddleware, this.billingController.deleteBilling);
-    this.router.get(`${this.path}/billing/:id`, authMiddleware, this.billingController.getBillingById);
+    this.router.post(`${this.path}/billing`, authMiddleware(['freelancer', 'client']), this.billingController.createBilling);
+    this.router.get(`${this.path}/billing`, authMiddleware(['freelancer', 'client']), this.billingController.getUserBilling);
+    this.router.put(`${this.path}/billing`, authMiddleware(['freelancer', 'client']), this.billingController.updateBilling);
+    this.router.delete(`${this.path}/billing/:id`, authMiddleware(['freelancer', 'client']), this.billingController.deleteBilling);
+    this.router.get(`${this.path}/billing/:id`, authMiddleware(['freelancer', 'client']), this.billingController.getBillingById);
 
     /*
     |--------------------------------------------------------------------------
     | Identity Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/identity`, authMiddleware, this.identityController.createIdentity);
-    this.router.get(`${this.path}/identity`, authMiddleware, this.identityController.getUserIdentity);
-    this.router.put(`${this.path}/identity`, authMiddleware, this.identityController.updateIdentity);
-    this.router.delete(`${this.path}/identity/:id`, authMiddleware, this.identityController.deleteIdentity);
-    this.router.get(`${this.path}/identity/:id`, authMiddleware, this.identityController.getIdentityById);
+    this.router.post(`${this.path}/identity`, authMiddleware(['freelancer', 'client']), this.identityController.createIdentity);
+    this.router.get(`${this.path}/identity`, authMiddleware(['freelancer', 'client']), this.identityController.getUserIdentity);
+    this.router.put(`${this.path}/identity`, authMiddleware(['freelancer', 'client']), this.identityController.updateIdentity);
+    this.router.delete(`${this.path}/identity/:id`, authMiddleware(['freelancer', 'client']), this.identityController.deleteIdentity);
+    this.router.get(`${this.path}/identity/:id`, authMiddleware(['freelancer', 'client']), this.identityController.getIdentityById);
 
     /*
     |--------------------------------------------------------------------------
     | Work Preference Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/preference`, authMiddleware, this.preferenceController.createPreference);
-    this.router.get(`${this.path}/preference`, authMiddleware, this.preferenceController.getUserPreference);
-    this.router.put(`${this.path}/preference`, authMiddleware, this.preferenceController.updatePreference);
-    this.router.delete(`${this.path}/preference/:id`, authMiddleware, this.preferenceController.deletePreference);
-    this.router.get(`${this.path}/preference/:id`, authMiddleware, this.preferenceController.getPreferenceById);
+    this.router.post(`${this.path}/preference`, authMiddleware(['freelancer', 'client']), this.preferenceController.createPreference);
+    this.router.get(`${this.path}/preference`, authMiddleware(['freelancer', 'client']), this.preferenceController.getUserPreference);
+    this.router.put(`${this.path}/preference`, authMiddleware(['freelancer', 'client']), this.preferenceController.updatePreference);
+    this.router.delete(`${this.path}/preference/:id`, authMiddleware(['freelancer', 'client']), this.preferenceController.deletePreference);
+    this.router.get(`${this.path}/preference/:id`, authMiddleware(['freelancer', 'client']), this.preferenceController.getPreferenceById);
 
     /*
     |--------------------------------------------------------------------------
     | About Route
     |--------------------------------------------------------------------------
     */
-    this.router.post(`${this.path}/about`, authMiddleware, this.aboutController.createAbout);
-    this.router.get(`${this.path}/about`, authMiddleware, this.aboutController.getUserAbout);
-    this.router.put(`${this.path}/about`, authMiddleware, this.aboutController.updateAbout);
-    this.router.delete(`${this.path}/about/:id`, authMiddleware, this.aboutController.deleteAbout);
-    this.router.get(`${this.path}/about/:id`, authMiddleware, this.aboutController.getAboutById);
+    this.router.post(`${this.path}/about`, authMiddleware(['freelancer', 'client']), this.aboutController.createAbout);
+    this.router.get(`${this.path}/about`, authMiddleware(['freelancer', 'client']), this.aboutController.getUserAbout);
+    this.router.put(`${this.path}/about`, authMiddleware(['freelancer', 'client']), this.aboutController.updateAbout);
+    this.router.delete(`${this.path}/about/:id`, authMiddleware(['freelancer', 'client']), this.aboutController.deleteAbout);
+    this.router.get(`${this.path}/about/:id`, authMiddleware(['freelancer', 'client']), this.aboutController.getAboutById);
   }
 }
 
