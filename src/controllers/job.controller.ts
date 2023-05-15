460577dbcd0df362065410ad9ef1ed4d7cf49148
olashina201 <olashina201@gmail.com>
2023-05-15 19:14:43 +0100

    update

--- src/controllers/job.controller.ts
+++ src/controllers/job.controller.ts
@@ -342,11 +342,13 @@ class JobController {
 
       // return user nips
       const bidders = await this.bidService.getAllBidders(job._id.toString());
-      const userIds = bidders.filter((bidder: any) => bidder.user_id.toString() !== user_id).map((bidder: any) => bidder.user_id.toString());
+      const userIds = bidders
+        .filter((obj: any) => obj.user_id !== user_id) // Exclude the object with the specified user_id
+        .map((obj: any) => obj.user_id);
 
       userIds.forEach(async (userId: any) => {
         await this.aboutService.updateAboutById(userId, { nips: +5 });
-        await this.bidService.(userId, { nips: +5 });
+        await this.bidService.updateBid(userId, { status: 'cancelled' });
       });
 
       res.status(200).json({ status: 200, response_code: 3000, message: 'JOB_REQUEST_SUCCESSFUL', data });
