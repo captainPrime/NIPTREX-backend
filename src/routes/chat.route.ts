81d70b25e4cfd83e26a5d013f529414a67783593
olashina201 <olashina201@gmail.com>
2023-05-18 23:05:14 +0100

    update

---
+++ src/routes/chat.route.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import { Routes } from '@interfaces/routes.interface';
+import ChatController from '@/controllers/chat.controller';
+import authMiddleware from '@/middlewares/auth.middleware';
+
+class ChatRoute implements Routes {
+  public path = '/chat';
+  public router = Router();
+  public chatController = new ChatController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}`, authMiddleware(['freelancer, client']), this.chatController.createChat);
+    this.router.get(`${this.path}/:id`, authMiddleware(['freelancer, client']), this.chatController.getChatById);
+    this.router.get(`${this.path}/user/:userId`, authMiddleware(['freelancer, client']), this.chatController.getChatsByUser);
+    this.router.post(`${this.path}/message`, authMiddleware(['freelancer, client']), this.chatController.createMessage);
+    this.router.get(`${this.path}/message/:chatId`, authMiddleware(['freelancer, client']), this.chatController.getMessagesByChat);
+  }
+}
+
+export default ChatRoute;
