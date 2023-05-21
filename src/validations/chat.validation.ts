304c387164d5508106060bb2b16c39ef068f76fa
olashina201 <olashina201@gmail.com>
2023-05-21 07:27:06 +0100

    update

--- src/validations/chat.validation.ts
+++ src/validations/chat.validation.ts
@@ -4,11 +4,13 @@ import Joi from 'joi';
 export const chatSchemaValidation = Joi.object({
   user1: Joi.string().required().label('User 1'),
   user2: Joi.string().required().label('User 2'),
+  milestone: Joi.string().required(),
 });
 
 // Message Validation Schema
 export const messageSchemaValidation = Joi.object({
   chat: Joi.string().required(),
+  milestone: Joi.string().required(),
   sender: Joi.string().required().label('Sender'),
   receiver: Joi.string().required().label('Receiver'),
   content: Joi.string().required().label('Content'),
