f15858c27a2a137e5f87905051c5122b98307c71
olashina201 <olashina201@gmail.com>
2023-06-14 05:20:25 +0100

    service model

---
+++ src/models/service.models.ts
@@ -0,0 +1,31 @@
+import { Schema, Document, model, Types } from 'mongoose';
+import { toJSON } from '@/modules/toJSON';
+
+export interface IService extends Document {
+  user_id: Types.ObjectId | string;
+  image: string[];
+  name: string;
+  level: number;
+  location: string;
+  description: string;
+  price: string;
+}
+
+const serviceSchema: Schema<IService> = new Schema(
+  {
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    image: [{ type: String, required: true }],
+    name: { type: String, required: true },
+    level: { type: Number, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true },
+  },
+  { versionKey: false },
+);
+
+serviceSchema.plugin(toJSON);
+
+export type IUpdateService = Partial<IService>;
+
+export const TransactionModel = model<IService>('Service', serviceSchema);
