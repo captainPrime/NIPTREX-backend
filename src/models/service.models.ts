2941d15d8c08efbb2ef3e44624df21f17a774c6f
olashina201 <olashina201@gmail.com>
2023-06-22 02:55:01 +0100

    update service schema

--- src/models/service.models.ts
+++ src/models/service.models.ts
@@ -3,23 +3,55 @@ import { toJSON } from '@/modules/toJSON';
 
 export interface IService extends Document {
   user_id: string | Types.ObjectId | any;
-  image: string[];
-  name: string;
-  level: string;
-  location: string;
+  title: string;
+  headline: string;
   description: string;
-  price: string;
+  projects: string[];
+  services: string[];
+  price: {
+    basic: {
+      rate: string;
+      duration: string;
+      services: string[];
+    };
+    standard: {
+      rate: string;
+      duration: string;
+      services: string[];
+    };
+    premium: {
+      rate: string;
+      duration: string;
+      services: string[];
+    };
+  };
 }
 
 const serviceSchema: Schema<IService> = new Schema(
   {
     user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    image: [{ type: String, required: true }],
-    name: { type: String, required: true },
-    level: { type: String, required: true },
-    location: { type: String, required: true },
+    title: { type: String, required: true },
+    headline: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: String, required: true },
+    projects: [{ type: String, required: true }],
+    services: [{ type: String, required: false }],
+    price: {
+      basic: {
+        rate: { type: String },
+        duration: { type: String },
+        services: [{ type: String }],
+      },
+      standard: {
+        rate: { type: String },
+        duration: { type: String },
+        services: [{ type: String }],
+      },
+      premium: {
+        rate: { type: String },
+        duration: { type: String },
+        services: [{ type: String }],
+      },
+    },
   },
   { versionKey: false },
 );
