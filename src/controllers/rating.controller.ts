a4770d1f08e3c1c819b28b024076011c5bab1ad6
olashina201 <olashina201@gmail.com>
2023-06-28 07:33:28 +0100

    update

--- src/controllers/rating.controller.ts
+++ src/controllers/rating.controller.ts
@@ -62,16 +62,16 @@ class RatingController {
 
   /*
   |--------------------------------------------------------------------------
-  | Get User Service
+  | Get User Rating
   |--------------------------------------------------------------------------
   */
   public getUserService = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id: string = req.user.id;
 
-      const data: IService[] | null = await this.serviceService.getServiceByUserId(id);
+      const data: IRating[] | null = await this.ratingService.getRatingByUserId(id);
 
-      res.status(200).json({ status: 200, response_code: 3000, message: 'SERVICE_REQUEST_SUCCESSFUL', data });
+      res.status(200).json({ status: 200, response_code: 8000, message: 'RATING_REQUEST_SUCCESSFUL', data });
     } catch (error) {
       next(error);
     }
@@ -79,19 +79,19 @@ class RatingController {
 
   /*
   |--------------------------------------------------------------------------
-  | Get Services
+  | Get Ratings
   |--------------------------------------------------------------------------
   */
-  public getAllServices = async (req: Request, res: Response, next: NextFunction) => {
+  public getAllRatings = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const options: PaginationOptions = {
-        sortBy: req.query.sortBy || 'date_posted:desc',
+        // sortBy: req.query.sortBy || 'date_posted:desc',
         limit: parseInt(req.query.limit as string, 10) || 5,
         page: parseInt(req.query.page as string, 10) || 1,
         projectBy: req.query.projectBy || 'name:hide, role:hide',
       };
 
-      const data: any[] | null = await this.serviceService.getAllService(req.query, options);
+      const data: any[] | null = await this.ratingService.getAllRating(req.query, options);
 
       res.status(200).json({ status: 200, response_code: 3000, message: 'SERVICE_REQUEST_SUCCESSFUL', data });
     } catch (error) {
@@ -104,7 +104,7 @@ class RatingController {
   | Update Service by ID
   |--------------------------------------------------------------------------
   */
-  public updateServiceById = async (req: Request, res: Response, next: NextFunction) => {
+  public unRateEntity = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const id: string = req.params.id;
       const body: Partial<IService> = req.body;
