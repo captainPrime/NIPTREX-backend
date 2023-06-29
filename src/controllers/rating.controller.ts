52a956a627ffee42385846611efafb0e8ffbac21
olashina201 <olashina201@gmail.com>
2023-06-29 11:56:30 +0100

    update

--- src/controllers/rating.controller.ts
+++ src/controllers/rating.controller.ts
@@ -23,9 +23,6 @@ class RatingController {
 
       const ratings = await this.ratingService.getRatingByUserId(entity);
 
-      console.log('RATINGS', ratings);
-
-      console.log('RATINGS LENGTH', calculateAverageRating(ratings?.length as number));
       await this.userService.updateUser(req.params.id, { rating: ratings?.length });
 
       res.status(200).json({ status: 200, response_code: 8000, message: 'RATING_REQUEST_SUCCESSFUL', data });
