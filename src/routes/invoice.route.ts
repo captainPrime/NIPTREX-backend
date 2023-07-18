716abac9d9f8db64bd8dbe7f2d12dd75d76b0936
olashina201 <olashina201@gmail.com>
2023-07-18 08:50:22 +0100

    invoice updated

---
+++ src/routes/invoice.route.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+
+import { Routes } from '@interfaces/routes.interface';
+import authMiddleware from '@/middlewares/auth.middleware';
+import InvoiceController from '@/controllers/invoice.controller';
+
+class InvoiceRoute implements Routes {
+  public path = '/invoice';
+  public router = Router();
+  public invoiceController = new InvoiceController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(`${this.path}`, authMiddleware(['freelancer', 'client']), this.invoiceController.createInvoice);
+    this.router.get(`${this.path}/getInvoiceById/:id`, authMiddleware(['freelancer', 'client']), this.invoiceController.getInvoiceById);
+    this.router.get(`${this.path}/getUserInvoice`, authMiddleware(['freelancer', 'client']), this.invoiceController.getUserInvoice);
+  }
+}
+
+export default InvoiceRoute;
